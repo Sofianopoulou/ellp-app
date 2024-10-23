5983352aef798314cdb9dcecbc512e1755ebc396
ala0702 <ala07022019@gmail.com>
2024-10-24 00:39:50 +0200

    feat: custom buttom and form field component / sign-in screen

--- components/FormField.tsx
+++ components/FormField.tsx
@@ -1,12 +1,77 @@
-import { View, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TextInputProps,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import React, { useState } from "react";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-const FormField = () => {
+interface FormFieldProps extends TextInputProps {
+  title: string;
+  value: string;
+  placeholder: string;
+  handleChangeText: (text: string) => void;
+  otherStyles?: object;
+}
+
+const FormField: React.FC<FormFieldProps> = ({
+  title,
+  value,
+  placeholder,
+  handleChangeText,
+  otherStyles,
+  ...props
+}) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <View className="space-y-2">
-      <Text>FormField</Text>
+    <View className={`space-y-2 m-3 ${otherStyles}`}>
+      <Text className="text-base text-gray-400 font-pmedium">{title}</Text>
+
+      <View className="w-full h-14 px-4 bg-black-100 rounded-2xl border-2 border-gray-400 focus:border-secondary flex flex-row items-center">
+        <TextInput
+          className="flex-1 color-text font-psemibold text-base"
+          value={value}
+          placeholder={placeholder}
+          placeholderTextColor="#7B7B8B"
+          onChangeText={handleChangeText}
+          secureTextEntry={title === "Password" && !showPassword}
+          {...props}
+        />
+
+        {title === "Password" && (
+          <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+            <Ionicons
+              name={showPassword ? "eye-off" : "eye"} // Icons for showing/hiding password
+              size={24}
+              color="#7B7B8B"
+            />
+          </TouchableOpacity>
+        )}
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 10,
+  },
+  label: {
+    fontSize: 16,
+    color: "#333333", // Replace 'color-text' with your desired color
+    marginBottom: 5,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    padding: 10,
+    borderRadius: 5,
+  },
+});
 
 export default FormField;
