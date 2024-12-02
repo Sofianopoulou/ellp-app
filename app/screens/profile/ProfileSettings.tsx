1e86c0e58af781c5a861b783247f13e3c2bd6d49
ala0702 <ala07022019@gmail.com>
2024-12-02 13:16:28 +0100

    Lias screen replacement for profileSetting

--- app/screens/profile/ProfileSettings.tsx
+++ app/screens/profile/ProfileSettings.tsx
@@ -9,38 +9,12 @@ import colors from "@/assets/colors/colors";
 import { auth } from "@/firebaseConfig";
 
 import SimpleAlert from "@/components/SimpleAlert";
-import Feather from "@expo/vector-icons/Feather";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import AntDesign from "@expo/vector-icons/AntDesign";
-import LargeButtonComponent from "@/components/LargeButtonComponent";
-import { useLayoutEffect, useState } from "react";
-import CustomAlert from "@/components/CustomAlert";
-import { useNavigation, useRouter } from "expo-router";
-
-import { auth, realtimeDb } from "@/firebaseConfig";
-import { deleteUser } from "firebase/auth";
-import { ref, remove } from "firebase/database";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { RootStackParamList } from "@/app/types/Navigation";
+import { useRouter } from "expo-router";
 import ProfileImageComponent from "@/components/Profile/ProfileImageComponent";
 import DeleteProfileComponent from "@/components/Profile/DeleteProfileComponent";
 import ProfileFieldsComponent from "@/components/Profile/ProfileFieldsComponent";
 
-type ProfileScreenNavigationProp = NativeStackNavigationProp<
-  RootStackParamList,
-  "Profile"
->;
-
 const ProfileSettings = () => {
-  const navigation = useNavigation<ProfileScreenNavigationProp>();
-
-  useLayoutEffect(() => {
-    navigation.setOptions({
-      title: "Edit profile",
-      headerBackButtonDisplayMode: "minimal",
-    });
-  }, [navigation]);
-
   const [isEditing, setIsEditing] = useState(false);
 
   const [simpleAlertVisible, setSimpleAlertVisible] = useState(false);
