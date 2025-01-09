667129cceca19c8439f041ddf4ab6c5fd79620ed
ala0702 <ala07022019@gmail.com>
2025-01-09 12:37:05 +0100

    work on favourite disconts screen

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -24,7 +24,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 // Export Firebase services
 export const auth = getAuth(app);
